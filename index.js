4fee7373305a0367834117d0600c12962e12f936
Angel Teyul <willson.alvarado@outlook.com>
2022-05-07 21:46:12 -0600

    code updated with user methods

--- index.js
+++ index.js
@@ -14,39 +14,151 @@ const PORT = 3000;
 // permitir acceso a la api
 app.use(cors());
 
+// test API funcionando
+
 app.get('/', (req, res) => {
   console.log(req.body);
 
   res.json({
-    message: 'Conexion exitosa con GET a home!'
+    msg: 'Conexion exitosa con GET a API'
   })
 })
 
-app.get('/test', (req, res) => {
-  console.log(req.body);
+// QUERYS_PERSONAS
 
-  res.json({
-    message: 'Conexion exitosa con GET a test!'
-  })
-})
-
-app.post('/testpost', (req, res) => {
-  console.log(req.body);
-  console.log('values:', Object.values(req.body))
-
+app.post('/insertar_usuario', (req, res) => {
   const values = Object.values(req.body)
-  var sql = 'insert into tbtest (code, test) values(?,?)';
+  var sql = 'insert into tbpersona (CodigoTipoPersona, CodigoEstado, Identificacion, PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, FechaNacimiento, Telefono, Direccion, CorreoElectronico, Contrasena) values(?,?,?,?,?,?,?,?,?,?,?,?)';
 
   db.query(sql, values, (err, data) => {
     if (err) {
       console.log(err);
+      res.json({
+        confirmation: false,
+        msg: "Usuario no agregado",
+        sqlmsg: err.sqlMessage,
+        error: err.sql
+      })
       return err;
     }
 
     res.json({
-      mensaje: 'agregado!',
-      data
+      confirmation: true,
+      msg: 'Usuario agregado con exito!'
     })
+  })
+})
+
+app.post('/actualizar_usuario', (req, res) => {
+  const values = Object.values(req.body)
+  var sql = 'update tbpersona set CodigoTipoPersona=?, CodigoEstado=?, Identificacion=?, PrimerNombre=?, SegundoNombre=?, PrimerApellido=?, SegundoApellido=?, FechaNacimiento=?, Telefono=?, Direccion=?, CorreoElectronico=?, Contrasena=? where CodigoPersona=?';
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        confirmation: false,
+        msg: "Datos no actualizados",
+        sqlmsg: err.sqlMessage,
+        error: err.sql
+      })
+      return err;
+    }
+
+    res.json({
+      confirmation: true,
+      msg: 'Datos actualizados con exito!'
+    })
+  })
+})
+
+app.post('/eliminar_usuario', (req, res) => {
+  console.log(req.body)
+  const values = Object.values(req.body)
+  var sql = 'update tbpersona set CodigoEstado=9 where CodigoPersona=?';
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        confirmation: false,
+        msg: "Usuario no eliminado",
+        sqlmsg: err.sqlMessage,
+        error: err.sql
+      })
+      return err;
+    }
+
+    res.json({
+      confirmation: true,
+      msg: 'Usuario eliminado con exito!'
+    })
+  })
+})
+
+app.get('/obtener_codigo_usuario', (req, res) => {
+  console.log(req.body)
+  const values = Object.values(req.body)
+  var sql = 'select CodigoPersona from tbpersona where CorreoElectronico=?';
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        confirmation: false,
+        msg: "Error al buscar usuario",
+        sqlmsg: err.sqlMessage,
+        error: err.sql
+      })
+      return err;
+    }
+
+    if (data[0] != undefined) {
+      res.json({
+        confirmation: true,
+        msg: 'Usuario encontrado con exito!',
+        CodigoPersona: data[0].CodigoPersona
+      })
+    } else {
+      res.json({
+        confirmation: false,
+        msg: 'Usuario no encontrado'
+      })
+    }
+  })
+})
+
+
+// LOGIN
+
+app.post('/login', (req, res) => {
+  const values = Object.values(req.body)
+  var sql = 'select CodigoPersona, TipoPersona, Estado, Identificacion, PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, FechaNacimiento, Telefono, Direccion, CorreoElectronico, Contrasena from tbpersona PE inner join tbtipopersona TP on PE.CodigoTipoPersona = TP.CodigoTipoPersona inner join tbestado ES on PE.CodigoEstado = ES.CodigoEstado where CorreoElectronico = ? and Contrasena = ?';
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        confirmation: false,
+        msg: "Error en login",
+        sqlmsg: err.sqlMessage,
+        error: err.sql
+      })
+      return err;
+    }
+
+    if (data[0] != undefined) {
+      res.json({
+        confirmation: true,
+        msg: 'Login completado!',
+        datos: data[0]
+      })
+    } else {
+      res.json({
+        confirmation: false,
+        msg: 'Datos incorrectos o usuario no existente'
+      })
+    }
   })
 })
 
